6a8efc096949c869fb5fd7360c65bb0fefc1f811
Serhii <serhiy.ilchenko@gmail.com>
2016-04-12 03:26:15 +0300

    hm 07

--- js/functions.js
+++ js/functions.js
@@ -60,12 +60,12 @@ function slidersInit(){
 		});
 	}
 
-	/*try-slider*/
-	var $testsSlider = $('.try-slider');
-	if($testsSlider.length){
-		$testsSlider.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+	/*exam slider*/
+	var $examSlider = $('.exam-slider');
+	if($examSlider.length){
+		$examSlider.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
 			//currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-			var $status = $(this).closest('.tests-slider').find('.try__counter');
+			var $status = $(this).closest('.tests-slider').find('.exam-counter');
 			var i = (currentSlide ? currentSlide : 0) + 1;
 			$status.text(i + ' из ' + slick.slideCount);
 		}).on('init', function (event, slick) {
